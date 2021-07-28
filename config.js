70bd316aa35edfbe57f9904bf9a8353f415b4aee
Alex <alex@localhost.localdomain>
2021-07-28 14:39:33 +0300

    initial commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const config = {
+    devAdminURL: "http://localhost:3001",
+    prodAdminURL: "https://www.admin.alispol.us",
+    devFrontendWebsiteURL: "http://localhost:3000",
+    prodFrontendWebsiteURL: "https://www.alispol.us"
+  }
+  
+  module.exports = config
